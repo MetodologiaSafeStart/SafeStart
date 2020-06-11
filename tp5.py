c549b191defd7b81a09c708b6ef197057e1ba043
oviedo22 <franco_oviedo_12@hotmail.com>
2020-06-11 09:42:57 -0300

    actualizacion tp5

--- tp5.py
+++ tp5.py
@@ -1,50 +1,50 @@
 
-tab = []
-loop = 'loop'
+i = int(input("Introduce La cantidad de numeros a evaluar Su Kaprekar "))
+for l in range(i):
+
+	tab = []
+	loop = 'loop'
 # La lista tab va a contener los números ingresados.
 # La variable loop va ser para una mejor redacción.
 
-def asc(n):
+	def asc(n):
     # Ordena los dígitos en forma ascendente...
-    return int(''.join(sorted(str(n))))
+		return int(''.join(sorted(str(n))))
 
-def desc(n):
+	def desc(n):
     # ...Ordena los dígitos de forma descendente
-    return int(''.join(sorted(str(n))[::-1]))
+		return int(''.join(sorted(str(n))[::-1]))
+	print(f"Ciclo numero: {l+1} ")
+	n = input("Introduce un numero de 4 dígitos: ")
+	n = int(n)
 
-n = input("Introduce un numero de 4 dígitos: ")
-n = int(n)
-
-if n>999 and n<10000:#Tiene que ser un número de 4 cifras.
- print("\nCargando...", str(n) + ":")
+	if n>999 and n<10000:#Tiene que ser un número de 4 cifras.
+		print("\nCargando...", str(n) + ":")
 
 
- while True:
+		while True:
     # Itera el programa, asignando la diferencia entre los valores.
-    print(desc(n), "-", asc(n), "=", desc(n)-asc(n))
-    n = desc(n) - asc(n)
+			print(desc(n), "-", asc(n), " =", desc(n)-asc(n))
+			n = desc(n) - asc(n)
 
-    if n not in tab:
+			if n not in tab:
         # Chequea si el numero aparece en tab o no, si no aparece lo agrega.
-        tab.append(n)
-    else:
-        if tab.index(n) == len(tab)-1:
+				tab.append(n)
+			else:
+				if tab.index(n) == len(tab)-1:
         # Si éste se encuentra al último, es una constante Kaprekar.
-            tab = []
-            tab.append(n)
-            loop = 'constante'
-        else:
+					tab = []
+					tab.append(n)
+					loop = 'constante'
+				else:
         
-            tab = tab[tab.index(n):]
-            
+					tab = tab[tab.index(n):]           
 
-        break
+				break
 
+			print('Kaprekar', loop, 'resultado:', tab)
  
- 
- print('Kaprekar', loop, 'resultado:', tab)
- 
-else:
- print("Número no válido")
+		else:
+			print("Número no válido")
 
 
