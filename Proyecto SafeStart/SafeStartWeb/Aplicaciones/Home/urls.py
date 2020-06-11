48cbfe8bf9e86dca2e39f8d7989f4c9e42d5a2f7
oviedo22 <franco_oviedo_12@hotmail.com>
2020-06-10 21:17:23 -0300

    Actualizacion

---
+++ Proyecto SafeStart/SafeStartWeb/Aplicaciones/Home/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path, re_path
+from . import views
+
+app_name ='home_app'
+
+urlpatterns=[
+	# primero url(si esta vacio, como en autores, entra al dominio principal 127.0.0.1:8000), luego vista a usar y luego un nombre 
+	path('nuevoProyecto',views.AddProyecto.as_view() , name="add-proyecto"),
+	#todo lo que este despues de url libros-autor/ hasta antes de otra / lo considere un pk y que recupere ese valor y lo guarde en ID
+	path('user/(?P<pk>\d+)$',views.MostrarUser.as_view(), name="user-perfil"),
+
+	path('proyectos',views.ListaProyecto.as_view(), name="lista-proyectos"),
+
+	path('proyecto/(?P<pk>\d+)$',views.MostrarProyecto.as_view(), name="proyecto-perfil"),
+
+
+ ]
---
+++ Proyecto SafeStart/SafeStartWeb/Aplicaciones/Home/views.py
@@ -0,0 +1,71 @@
+from django.shortcuts import render
+from django.views.generic import(ListView,
+	CreateView, DetailView,
+	)
+# Create your views here.
+
+from .models import Usuario, Proyecto
+
+class AddProyecto(CreateView):
+	template_name='home/add-proyecto.html'
+	model= Proyecto
+	second_model=Usuario
+	nombre_usuario= nombre
+	fields=['nombre_proyecto','descripcion_proyecto',
+	'foto_proyecto','foto_proyecto2','fecha_publicacion',
+	'nombre_usuario','rubro']
+	success_url='/'
+
+class AddProyecto(CreateView):
+	template_name='home/add-proyecto.html'
+	model= Proyecto
+	second_model=Usuario
+	nombre_usuario= nombre
+	fields=['nombre_proyecto','descripcion_proyecto',
+	'foto_proyecto','foto_proyecto2','fecha_publicacion',
+	'nombre_usuario','rubro']
+	success_url='/'
+
+class ListaProyecto(ListView):
+	template_name='home/proyectos.html'
+	model= Proyecto
+	context_object_name=proyectos
+	paginate_by = 10
+	def get_queryset(self):
+		queryset = super(ListaProyecto, self).get_queryset()
+		fecha = date.today()
+		if self.request.GET.get('fecha'):
+			from datetime import datetime
+			fecha_str = self.request.GET.get('fecha')
+			fecha = datetime.strptime(fecha_str, '%d-%m-%Y')
+		return queryset.filter(fecha_publicacion =fecha)
+
+class MostrarUser(DetailView):
+	template_name='home/user.html'
+	model=Usuario
+	context_object_name=user
+
+	def get_queryset(self):
+		id = self.kwargs['nombre']
+		lista=Usuario.objects.filter(
+			nombre_usuario=id
+
+			)
+		return lista
+
+class MostrarProyecto(DetailView):
+	template_name='home/proyecto.html'
+	context_object_name=proyecto
+
+	def get_queryset(self):
+		id = self.kwargs['pk']
+		lista=Proyecto.objects.filter(
+			Proyecto=id
+
+			)
+		return lista
+
+
+
+
+
