1901ba3b22711da7ca6eea8c30c0cf491d267f89
Martin <martinmonteavaro@gmail.com>
2020-06-30 00:00:08 -0300

    Actualizacion

--- Proyecto SafeStart/SafeStartWeb/Aplicaciones/Home/models.py
+++ Proyecto SafeStart/SafeStartWeb/Aplicaciones/Home/models.py
@@ -1,27 +1,29 @@
 from django.db import models
-from registration.users import UserModel
-from registration.users import UsernameField
-
-User = UserModel()
-
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 class Usuario(models.Model):#TABLA DE LA BASE DE DATOS
 
-    nombre = models.CharField('Nombre',max_length=25,help_text='Ingrese su nombre',null=True,blank=True)
     fecha_nacimiento =models.DateField(help_text='Fecha de nacimiento',null=True,blank=True)
     foto_perfil=models.ImageField(help_text='Elije una foto',null=True,blank=True)
     profesion=models.CharField(help_text='Profesión',max_length=30,null=True,blank=True)
-    correo=models.EmailField(help_text='Ingrese su correo')
     presentacion=models.TextField(help_text='Acerca de ti',max_length=200,null=True,blank=True)
     enlace_referencias=models.CharField(help_text='Link',max_length=50,null=True,blank=True)
-    User= models.OneToOneField(
-        User(),null= True,
-        on_delete=models.CASCADE
-    )
+    user= models.OneToOneField(User,on_delete=models.CASCADE)
+     
+    
 
     def __str__(self):
+      return self.usuario.username
 
-        return self.nombre
+def crear_usuario_perfil(sender,instance,created,**kwargs):
+  if created:
+    Usuario.objects.create(user=instance)
+
+def guardar_usuario_perfil(sender,instance,created,**kwargs):
+  instance.usuario.save()
+
 
 
 class Proyecto(models.Model):
@@ -77,13 +79,3 @@ class Proyecto(models.Model):
   
     def __str__(self):
         return self.nombre_proyecto
-
-
-
-
-    
-
-
-   
-    
-
--- Proyecto SafeStart/SafeStartWeb/Aplicaciones/Home/urls.py
+++ Proyecto SafeStart/SafeStartWeb/Aplicaciones/Home/urls.py
@@ -1,4 +1,3 @@
-
 from django.urls import path, re_path
 from . import views
 
@@ -10,7 +9,7 @@ urlpatterns=[
 
 	path('categorias',views.Categorias.as_view(), name="categorias"),
 
-	path('login',views.Login.as_view(), name="login"),
+	path('registrate',views.SignUpView.as_view(), name="sign_up"),
 
 	path('como-funciona',views.ComoFunciona.as_view(), name="como-funciona"),
 
@@ -29,6 +28,14 @@ urlpatterns=[
 	path('modify-user/<pk>',views.ModifyUser.as_view(), name="modify-user"),
 
 	path('proyectos/<slug>',views.ProyectoCategoria.as_view(), name="proyecto-categoria"),
+
+	path('iniciar-sesion/', views.SignInView.as_view(), name='sign_in'),
+	
+	path('cerrar-sesion/', views.SignOutView.as_view(), name='sign_out'),
+	
+	path('perfil/', views.AddPerfil.as_view(), name='perfil'),
+
+
 
 	
 
